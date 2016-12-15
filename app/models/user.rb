f826f012a23b0a848d5b5df139753659579721a8
Will Whitmey <w.whitmey@outlook.com>
2016-12-15 18:24:39 +0000

    heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :password_confirmation, presence: true, on: :create
-  has_many :profiles
-  has_many :reviews
+  has_many :profiles, depended: :destroy
+  has_many :reviews, depended: :destroy
 end
