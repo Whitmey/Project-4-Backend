25bb0a30e8e1ea581c98b82e5a0c2ca2d334d17e
Will Whitmey <w.whitmey@outlook.com>
2016-12-15 18:44:57 +0000

    Updated user model again

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :password_confirmation, presence: true, on: :create
-  has_many :profiles, dependant: :destroy
-  has_many :reviews, dependant: :destroy
+  has_many :profiles, dependent: :destroy
+  has_many :reviews, dependent: :destroy
 end
